21f993ce762eaf91884bc9287d37b5cb2bdffbc0
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-11-04 11:34:12 -0800

    handling dynamic content the JS way

--- src/App.js
+++ src/App.js
@@ -49,6 +49,25 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
 
     return (
       <div className="App">
@@ -56,23 +75,8 @@ class App extends Component {
         <p>This is really working!</p>
         <button 
           style={style}
-          onClick={() => this.togglePersonsHandler()}>Switch Name</button>
-          { 
-            this.state.showPersons === true?
-              <div>
-                <Person
-                  name={this.state.persons[0].name} 
-                  age={this.state.persons[0].age}/>
-                <Person
-                  name={this.state.persons[1].name} 
-                  age={this.state.persons[0].age}
-                  click={this.switchNameHandler.bind(this, 'Max!')}
-                  changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-                <Person
-                  name={this.state.persons[2].name} 
-                  age={this.state.persons[2].age} />
-              </div> : null
-          }
+          onClick={() => this.togglePersonsHandler}>Switch Name</button>
+          { persons }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
