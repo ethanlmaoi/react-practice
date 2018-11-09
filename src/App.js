6c4a15cecce8cca3012c152be354d406f0171ed4
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-11-09 11:51:54 -0800

    new practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -47,14 +47,6 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
 
     let persons = null;
 
@@ -75,20 +67,20 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push( classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi I Am React</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button 
-            style={style}
+            stylxe={style}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
             { persons }
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,12 +3,6 @@ import './Person.css';
 import Radium from 'radium'
 
 const person = ( props ) => {
-    const style = {
-        '@media (min-width: 500px': {
-            width: '450px'
-        }
-    };
-
     return (
         <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
@@ -18,4 +12,4 @@ const person = ( props ) => {
     )
 };
 
-export default Radium(person);
+export default person;
